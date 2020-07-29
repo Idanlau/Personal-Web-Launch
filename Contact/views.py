b5e9fde3c80fb63dadf88c2d7fd077ce4ffe3543
idanlau <lauidan31@gmail.com>
2020-07-29 15:09:45 +0800

    commit message

---
+++ Contact/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
+from Accounts.models import Profile
+from django.contrib.auth.models import User
+
+def contactview(request):
+    email=''
+    subject=''
+    message=''
+
+
+    form= ContactForm(request.POST or None, initial={'email': request.user.email})
+
+    if request.user.is_authenticated:
+
+        if form.is_valid():
+            email= form.cleaned_data.get("email")
+            subject= form.cleaned_data.get("subject")
+            message=form.cleaned_data.get("message")
+
+
+            subject= str(request.user) + "'s Comment"
+
+
+            message = email + " with the email, " + subject + ", sent the following message:\n\n" + message
+            send_mail(subject, message, email, ['lauidan31@gmail.com'])
+
+            return render(request, 'contact.html', {'form': form, 'profile': Profile.objects.get(user=request.user)})
+
+
+        else:
+            return render(request, 'contact.html', {'form': form, 'profile': Profile.objects.get(user=request.user)})
+
+    else:
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            subject = form.cleaned_data.get("subject")
+            message = form.cleaned_data.get("message")
+
+            subject = "Guest" + "'s Comment"
+
+            message = email + " with the email, " + subject + ", sent the following message:\n\n" + message
+            send_mail(subject, message, email, ['lauidan31@gmail.com'])
+
+        return render(request, 'not_logged_contact.html',{'form': form})
+
+
+
+
+
---
+++ IdanLauSite/urls.py
@@ -0,0 +1,40 @@
+"""IdanLauSite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLcid = models.AutoField(primary_key=True, **options)nf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path
+from Accounts.views import account_view, account_create_view, logout_view, edit_profile_view, CustomLoginView
+from Pages.views import home_view, preview, post_view, not_logged_preview
+from Upload.views import AdminPostView
+from Contact.views import contactview
+
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_view),
+    path('create/', account_create_view),
+    path('account/profile/', account_view),
+    path('logout/', logout_view),
+    path('prev/', preview.as_view()),
+    path('post/<id>/', post_view),
+    path('edit/bio/', edit_profile_view),
+    path('login/', CustomLoginView.as_view()),
+    path('upload/',AdminPostView),
+    path('not/prev/', not_logged_preview.as_view()),
+    path('contact/', contactview)
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
